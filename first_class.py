7b295fec0bd180ff6f32c6ea5bbb4f9df5757a33
olawhales <ajaditaoreed@gmail.com>
2024-07-09 11:47:12 +0100

    python testing

---
+++ first_class.py
@@ -0,0 +1,16 @@
+print("hello world")
+
+first = 20
+second = 40
+print(first + second)
+
+
+first = 100
+second = 200
+result = first + second
+print(result)
+
+first = 500
+second = 300
+result = first - second
+print(result)
