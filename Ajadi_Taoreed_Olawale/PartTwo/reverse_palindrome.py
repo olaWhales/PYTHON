7e68104a6ca1bf3c0cea870903829f6fe6832924
olawhales <ajaditaoreed@gmail.com>
2024-07-30 11:21:15 +0100

    adding something

--- Ajadi_Taoreed_Olawale/PartTwo/reverse_palindrome.py
+++ Ajadi_Taoreed_Olawale/PartTwo/reverse_palindrome.py
@@ -1,3 +1,10 @@
+#prompt user to enter three integer
+#check the first number by dividing by 1 and modulo to get the remainder
+#check the second number by dividing by 10 and modulo to get the remainder
+#check the first number by dividing by 100 and modulo to get the remainder
+#compare the first number with the last number if both are the same (it's says, it's a pallindrome(TRUE)) and if not the same (means not a pallindrome(FALSE))
+
+
 def reverse(number):
 	user1 = number // 1 %10
 	user2 = number // 10 %10
