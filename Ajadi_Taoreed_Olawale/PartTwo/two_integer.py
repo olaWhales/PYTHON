e7badb8e73c814ed0c33ec128e5b7d9cd3774c6f
olawhales <ajaditaoreed@gmail.com>
2024-07-30 11:58:57 +0100

    adding unfinish one

---
+++ Ajadi_Taoreed_Olawale/PartTwo/two_integer.py
@@ -0,0 +1,11 @@
+#assign a variable to receive input from the user
+#use a python keyword called count to count the input
+#print the input
+
+def two_integer(letter):
+	result = letter.count
+	return result
+
+
+print(two_integer("corhort"))
+	
