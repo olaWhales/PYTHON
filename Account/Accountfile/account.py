20e6b97b01336146322f42f28b64e97dbd89ed2d
olawhales <ajaditaoreed@gmail.com>
2024-09-25 21:59:03 +0100

    updates

--- Account/Accountfile/account.py
+++
@@ -1,7 +0,0 @@
-class Account:
-    account_number = 0
-    def __init__(self , first_name, last_name , pin , balance, ):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.pin = pin
-        self.balance = balance
--- Account/test/test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from Accountfile import Element_sorting
-
-
-class TestVariableSort(unittest.TestCase):
-    def test_that_element_sorting_exist(self):
-        self.element_sorting.element()
-        assertEquals(True)
